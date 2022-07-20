1e125ef02d7b320c30d64b63acc3061cafb1d6ff
AlexKorshakov <korshakov_07@mail.ru>
2022-07-20 09:04:19 +0300

    v0.6 add css, html, js

---
+++ application/config/static/scripts/background_by_value.js
@@ -0,0 +1,11 @@
+var elements = document.getElementsByClassName("card");
+
+var i;
+
+// List View
+function myFunction() {
+  for (i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundColor = '#98e7e6'
+
+  }
+}
