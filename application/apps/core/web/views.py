944e66bf31ed0406c1d7386520c87bc300c61327
AlexKorshakov <korshakov_07@mail.ru>
2022-08-04 01:40:55 +0300

    v1.7 in view add class ViolationsByStatus def register def user_login def user_logout

--- application/apps/core/web/views.py
+++ application/apps/core/web/views.py
@@ -5,40 +5,39 @@ from pprint import pprint
 from django.http import HttpRequest, HttpResponse
 from django.core.paginator import Paginator
 from django.views.generic import ListView, DetailView, CreateView
+from django.shortcuts import render, get_object_or_404, redirect
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.contrib.auth import login, logout
+
 from apps.core.web.utils import MyMixin, del_violations, get_id_registered_users, get_params
 from loader import logger
 
-from django.shortcuts import render, get_object_or_404, redirect
+from .forms import UsrRegisterForm, UserLoginForm
 
 # from .models import News
-from .models import Violations, MainCategory, Location, GeneralContractor, IncidentLevel
+from .models import Violations, MainCategory, Location, GeneralContractor, IncidentLevel, Status
 
 # Create your views here.
-from ..bot.database.DataBase import run
+from ..bot.database.DataBase import upload_from_local
 
 
 def upload(request: HttpRequest):
     #  https://ru.stackoverflow.com/questions/1313580/Запуск-функции-при-нажатии-кнопки-django
 
     if request.method == 'POST':
-        content = asyncio.run(get_id_registered_users())
-        params = asyncio.run(get_params(content))
-        logger.info(content)
-
-        # user_chat_id = '373084462'
-        # params: dict = {
-        #     'all_files': True,
-        #     'file_path': f"C:/Users/KDeusEx/PycharmProjects/SWTS/application/media/{user_chat_id}/data_file/",
-        #     'user_file': f"C:/Users/KDeusEx/PycharmProjects/SWTS/application/media/{user_chat_id}/{user_chat_id}.json"
-        # }
+        content: list = asyncio.run(get_id_registered_users())
+        params: list = asyncio.run(get_params(content))
+        logger.debug(content)
 
         for param in params:
-            asyncio.run(run(params=param))
+            asyncio.run(upload_from_local(params=param))
             logger.info(f'Данные загружены в БД')
         return redirect('home')
 
 
-def upload_violations(request: HttpRequest):
+def update_violations(request: HttpRequest):
     if request.method == 'POST':
         logger.info(f'запись обновлена')
         return redirect('home')
@@ -48,7 +47,7 @@ def delete_violations(request):
     violation_file_id = request.POST['violation_file_id']
 
     if request.method == 'POST':
-        logger.info(violation_file_id)
+        logger.debug(violation_file_id)
         result = asyncio.run(del_violations(violation_file_id))
         if result:
             logger.info(f'запись {violation_file_id} удалена')
@@ -181,3 +180,59 @@ class ViolationsByIncidentLevel(MyMixin, ListView):
             incident_level_id=self.kwargs['incident_level_id'],
             is_published=True
         ).prefetch_related('incident_level')
+
+
+class ViolationsByStatus(MyMixin, ListView):
+    """Просмотр категории"""
+    model = Violations
+    template_name = 'home_violations_list.html'
+    context_object_name = 'violations'
+    paginate_by = 20
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        """Дополнение данных перед отправкой на рендер"""
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.get_upper(Status.objects.get(pk=self.kwargs['status_id']))
+        logger.debug(f"{context=}")
+        return context
+
+    def get_queryset(self):
+        return Violations.objects.filter(
+            status_id=self.kwargs['status_id'],
+            is_published=True
+        ).prefetch_related('status')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UsrRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Вы успешно зарегистрировались')
+            return redirect("home")
+        else:
+            messages.error(request, 'Ошибка регистрации')
+
+    else:
+        form = UsrRegisterForm()
+
+    return render(request, 'news/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = UserLoginForm()
+
+    return render(request, 'news/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("login")
